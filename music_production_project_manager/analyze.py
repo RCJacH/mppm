4a9907bfe7568225b14e0e785016271589e2b865
RCJacH <RCJacH@outlook.com>
2020-02-03 23:50:25 +0800

    Imported Analyze from older Monolizer project

---
+++ music_production_project_manager/analyze.py
@@ -0,0 +1,128 @@
+import numpy as np
+
+np.seterr(divide="ignore", invalid="ignore")
+from soundfile import SoundFile as sf
+from soundfile import SEEK_END
+
+
+class SampleblockChannelInfo:
+    def __init__(
+        self,
+        threshold=0.00001,
+        flag=0,
+        isCorrelated=None,
+        sample=[],
+        sampleblock=None,
+        noisefloor=0,
+    ):
+        """Analyze audio information in a single sampleblock
+        
+        Keyword Arguments:
+            threshold {float} -- Ignore all samples below this value (default: {0.00001})
+            flag {int} -- Previous channel flag info (default: {0})
+            isCorrelated {[type]} -- Previous channel panning info (default: {None})
+            sample {list} -- Previous sample value (default: {[]})
+            sampleblock {[type]} -- sampleblock to analyze (default: {None})
+            noisefloor {int} -- Previous noisefloor info (default: {0})
+
+        Returns:
+        flag -- Indicate which channel has valid sounding sample
+        isCorrelated -- The panning if both channels have fixed ratio
+        sample -- A valid sample of each channel of the same position
+        noisefloor -- The lowest bit that contains information
+        """
+        self.flag = flag != None and flag
+        self.isCorrelated = isCorrelated
+        self.sample = sample != None and sample
+        self.NULL_THRESHOLD = threshold
+        self.noisefloor = noisefloor
+        if sampleblock is not None:
+            self.set_info(sampleblock)
+
+    def set_info(self, sampleblock):
+        self.set_channelblock(sampleblock)
+        self.set_flag()
+        self.set_correlation(sampleblock)
+        self.set_sample(sampleblock)
+        self.set_noisefloor(sampleblock)
+
+    def set_channelblock(self, sampleblock):
+        self.channelblock = self._transpose(sampleblock)
+
+    def _transpose(self, sampleblock):
+        return np.array(sampleblock).transpose(1, 0)
+
+    def set_flag(self):
+        [
+            self.flag_on(i + 1)
+            for i, v in enumerate(self.channelblock)
+            if any(x != 0 for x in v)
+        ]
+        return self.flag
+
+    def flag_on(self, n):
+        try:
+            self.flag |= 1 << (n - 1)
+        except ValueError:
+            self.flag = 0
+        return self.flag
+
+    def set_correlation(self, channelblock):
+        """Check whether audio is panned mono"""
+        if self.isCorrelated != False:
+            self.isCorrelated = (
+                True
+                if len(channelblock) == 1
+                else self._is_sampleblock_correlated(channelblock)
+            )
+
+    def _is_sampleblock_correlated(self, channelblock):
+        ratios = [self._get_ratio(samples) for samples in channelblock]
+        return self._is_ratio_correlated(ratios)
+
+    def _get_ratio(self, samples):
+        a = np.array(samples[:-1])
+        b = np.array(samples[1:])
+        return np.nan_to_num(np.divide(b, a, dtype="float")).flatten()
+
+    def _is_ratio_correlated(self, ratios):
+        return (np.absolute(np.subtract(*ratios)).flat < self.NULL_THRESHOLD).all()
+
+    def set_sample(self, sampleblock):
+        self.sample = self._get_sample_from_sampleblock(sampleblock)
+
+    def reset_sample(self):
+        self.sample = []
+
+    def _get_sample_from_sampleblock(self, sampleblock):
+        sample = self.sample
+        if not sample or not self._is_sample_stereo(sample):
+            sample = self._get_valid_sample(sampleblock)
+        return sample
+
+    def _is_sample_stereo(self, sample):
+        return len(set(sample)) > 1
+
+    def _get_valid_sample(self, sampleblock):
+        try:
+            return next(
+                (
+                    self._validate_sample(samples)
+                    for samples in sampleblock
+                    if (0 not in samples and len(samples) > 0)
+                )
+            )
+        except StopIteration:
+            return self.sample
+
+    def _validate_sample(self, samples):
+        try:
+            return samples.tolist()
+        except AttributeError:
+            return samples
+
+    def set_noisefloor(self, channelblock):
+        self.noisefloor = min(self.noisefloor or self._get_noisefloor_from_sampleblock(channelblock))
+
+    def _get_noisefloor_from_sampleblock(self, channelblock):
+        return [min(abs(x)) for x in channelblock]
---
+++ tests/test_analyze.py
@@ -0,0 +1,181 @@
+"""
+Tests for 'analyze' module
+"""
+import pytest
+import os
+import numpy as np
+from music_production_project_manager.analyze import SampleblockChannelInfo
+from soundfile import read
+
+
+class Test_SampleblockChannelInfo(object):
+    @pytest.fixture
+    def obj(self):
+        return SampleblockChannelInfo()
+
+    def test_flag_on_from_sample(self, obj):
+        obj.flag = 0
+        obj.channelblock = [[0], [0.5]]
+        assert obj.set_flag() == 2
+        obj.flag = 0
+        obj.channelblock = [[-0.25], [0]]
+        assert obj.set_flag() == 1
+        obj.flag = 0
+        obj.channelblock = [[0.0], [0.0]]
+        assert obj.set_flag() == 0
+
+    def test_flag_on(self, obj):
+        obj.flag = 0
+        assert obj.flag == 0
+        assert obj.flag_on(1) == 1
+        assert obj.flag_on(1) == 1
+        assert obj.flag_on(2) == 3
+        obj.flag = 0
+
+    def test_transpose(self, obj):
+        assert (
+            obj._transpose([[0.1, 0.2], [0.3, 0.6], [0.15, 0.3]])
+            == [[0.1, 0.3, 0.15], [0.2, 0.6, 0.3]]
+        ).all()
+
+    def test_get_ratio(self, obj):
+        assert obj._get_ratio([0.5, 0.5]) == 1
+        assert obj._get_ratio([0.22900572, 0.11450286]) == 0.5
+        assert obj._get_ratio([0.0, 0.0]) == 0
+
+    def test_is_ratio_correlated(self, obj):
+        assert obj._is_ratio_correlated([[1], [1]]) == True
+        assert (
+            obj._is_ratio_correlated(
+                [
+                    [0.99990839, 0.99987785, 0.99987783, 0.99984727],
+                    [0.998004, 0.99800001, 0.99799599, 0.99799196],
+                ]
+            )
+            == False
+        )
+        assert (
+            obj._is_ratio_correlated(
+                [
+                    [1.0148026, 1.01443943, 1.01423377],
+                    [1.0148026, 1.01443943, 1.01423382],
+                ]
+            )
+            == True
+        )
+        assert obj._is_ratio_correlated([[1.0, 1.0], [1.0, 1.000009],]) == True
+        assert obj._is_ratio_correlated([[1, 2], [1, 2.00001]]) == False
+
+    def test_is_sampleblock_correlated(self, obj):
+        assert obj._is_sampleblock_correlated([[0.5, 0.5, 0.5]] * 2) == True
+        assert (
+            obj._is_sampleblock_correlated(
+                [[0.00308228, 0.00613403], [0.00308228, 0.00613403],]
+            )
+            == True
+        )
+        assert (
+            obj._is_sampleblock_correlated(
+                [
+                    [0.22900572, 0.2323956, 0.23575126, 0.23910689],
+                    [0.11450286, 0.1161978, 0.11787563, 0.11955345],
+                ]
+            )
+            == True
+        )
+        assert (
+            obj._is_sampleblock_correlated(
+                [
+                    [0.99996948, 0.99996948, 0.99996948],
+                    [0.99996948, 0.99804688, 0.99609375],
+                ]
+            )
+            == False
+        )
+        assert obj._is_sampleblock_correlated([[0.0, 0.0, 0.0]] * 2) == True
+
+    def test_get_sample_from_sampleblock(self, obj):
+        obj.reset_sample()
+        assert obj.sample == []
+        assert obj._get_sample_from_sampleblock([[0.0, 0.0]] * 3) == []
+        assert obj._get_sample_from_sampleblock(
+            [[0.00308228, 0.00308228], [0.00613403, 0.00613403]]
+        ) == [0.00308228, 0.00308228]
+        assert obj._get_sample_from_sampleblock(
+            [[0.99996948, 0.0], [0.99996948, 0.99804688], [0.99996948, 0.99609375]]
+        ) == [0.99996948, 0.99804688]
+        obj.sample = [0.1, 0.2]
+        assert obj._get_sample_from_sampleblock([[0.2, 0.5,]]) == [0.1, 0.2]
+        obj.sample = [0.1, 0.1]
+        assert obj._get_sample_from_sampleblock([[0.2, 0.5,]]) == [0.2, 0.5]
+        assert obj._get_sample_from_sampleblock([[]]) == [0.1, 0.1]
+        obj.reset_sample()
+
+    def test_is_sample_stereo(self, obj):
+        assert obj._is_sample_stereo([0.5, 0.3]) == True
+        assert obj._is_sample_stereo([0.5]) == False
+        assert obj._is_sample_stereo([0.5, 0.5]) == False
+        assert obj._is_sample_stereo([0, 0]) == False
+
+    def test_noisefloor(self, obj):
+        assert obj._get_noisefloor_from_sampleblock([[0.2, 1], [0.1, 0]]) == [0.2, 0]
+        assert obj._get_noisefloor_from_sampleblock([[0, 0], [0, 0]]) == [0, 0]
+
+
+# class AudioInfo:
+#     def __init__(self, shape):
+#         self.src = Monolizer()
+#         self.src.file = "tests//" + shape + ".wav"
+#         if shape == "sin":
+#             self.channel = 0
+#             self.channels = 1
+#             self.isMono = True
+#             self.isEmpty = False
+#             self.isFakeStereo = False
+#         if shape == "sins":
+#             self.channel = 0
+#             self.channels = 2
+#             self.isMono = True
+#             self.isEmpty = False
+#             self.isFakeStereo = True
+#         if shape == "empty":
+#             self.channel = self.src.EMPTY
+#             self.channels = 1
+#             self.isMono = False
+#             self.isEmpty = True
+#             self.isFakeStereo = False
+#         if shape == "sin_tri":
+#             self.channel = self.src.STEREO
+#             self.channels = 2
+#             self.isMono = False
+#             self.isEmpty = False
+#             self.isFakeStereo = False
+#         if shape == "sin_l50":
+#             self.channel = 0
+#             self.channels = 2
+#             self.isMono = True
+#             self.isEmpty = False
+#             self.isFakeStereo = True
+#         if shape == "sin_r25":
+#             self.channel = 1
+#             self.channels = 2
+#             self.isMono = True
+#             self.isEmpty = False
+#             self.isFakeStereo = True
+#         if shape == "sin_r100":
+#             self.channel = 1
+#             self.channels = 2
+#             self.isMono = True
+#             self.isEmpty = False
+#             self.isFakeStereo = True
+
+#     def __enter__(self):
+#         return self
+
+#     def __exit__(self, *args):
+#         del self.src
+
+# @pytest.fixture(params=["sin", "sins", "empty", "sin_tri", "sin_l50", "sin_r25", "sin_r100"])
+# def audioinfo(request):
+#     with AudioInfo(request.param) as info:
+#         yield info
