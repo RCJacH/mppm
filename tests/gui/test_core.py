0e4e4bf97ab0d8a7679c7ed8a6bb844f1ba58dc8
RCJacH <RCJacH@outlook.com>
2020-03-09 21:43:40 +0800

    Add Settings to Core

--- tests/gui/test_core.py
+++ tests/gui/test_core.py
@@ -1,15 +1,17 @@
 import pytest
 from music_production_project_manager.gui.style import core
 
-# def test_search():
-#     @core.search
-#     def sample(data, key, *args, **kwargs):
-#         pass
-#     assert sample({"a": {"aa": 1}}, "a.aa", "arg") == 1
+
 class ColorSample:
     def __init__(self):
         self.themes = self.setup_theme()
         self.properties = self.setup_properties()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args):
+        del self
 
     def setup_theme(self):
         themes = (
@@ -52,45 +54,67 @@ class ColorSample:
 class TypographySample:
     def __init__(self):
         self.tag = self.setup_tag()
+        self.fontface = "fontface"
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args):
+        del self
 
     def setup_tag(self):
         tags = ("h1", "h2", "h3", "h4", "h5", "h6", "p", "li", "ul", "ol")
         return {x: [f"style_{x}", f"size_{x}"] for x in tags}
 
+    def asdict(self):
+        typo = {"tag": self.tag, "fontface": self.fontface}
+        return {"typography": typo}
 
-color = ColorSample()
-typography = TypographySample()
+
+@pytest.fixture(scope="module")
+def color():
+    with ColorSample() as o:
+        yield core.Style(o.asdict())
+
+@pytest.fixture(scope="module")
+def typography():
+    with TypographySample() as o:
+        yield core.Style(o.asdict())
 
 
 class TestStyle:
-    def __init__(self):
-        self.color = core.Style(color.asdict())
-        self.typography = core.Style(typography.asdict())
-
     @pytest.mark.parametrize("key", ["black", "gray_100", "red_20",])
-    def test_color_theme(self, key):
-        assert self.color.color(key) == f"#{key}"
+    def test_color_theme(self, color, key):
+        assert color.color(key) == f"#{key}"
 
     @pytest.mark.parametrize(
         "key, value",
         [
-            ("a.a", "aaa"),
+            ("a.aa", "aaa"),
             ("a.ab.aba", "#black"),
             ("b.ba", "#gray_20"),
             ("b.bb.bba", "#blue_10"),
         ],
     )
-    def test_color_property(self, key, value):
-        assert self.color.color(key) == value
+    def test_color_property(self, color, key, value):
+        assert color.color(key) == value
 
     @pytest.mark.parametrize("key", ["#000", "Other"])
-    def test_color_other(self, key):
-        assert self.color.color(key) == key
+    def test_color_other(self, color, key):
+        assert color.color(key) == key
 
     @pytest.mark.parametrize("key", ["h1", "h5", "p", "ol"])
-    def test_typography(self, key):
-        assert self.typography.typography(key) == [f"style_{key}", f"size_{key}"]
+    def test_typography(self, typography, key):
+        assert typography.typography(key) == key
 
-    @pytest.mark.parametrize("key", ["h2", "h3", "li"])
-    def test_typography_font(self, key):
-        assert self.typography.fonttag(key) == [f"style_{key}", f"size_{key}"]
+    @pytest.mark.parametrize("key", ["h4", "h5", "ul"])
+    def test_typography_fonttag(self, typography, key):
+        assert typography.fonttag(key) == [f"style_{key}", f"size_{key}"]
+
+    def test_typography_fontface(self, typography):
+        assert typography.fontface() == "fontface"
+
+class TestConfigure:
+
+    def test_pass(self):
+        pass
